a54a4ee6366c84b6dfc8e36a17559ffc1084cac2
Yanuario Bagas Prayoga <contact@kakandazyan.my.id>
2022-10-04 16:03:06 +0700

    Update kasus1_1.java

--- kasus1_1.java
+++ kasus1_1.java
@@ -1,1 +1,14 @@
+import java.util.*;
+import java.lang.Math;
 
+public class kasus1_1 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+        
+        System.out.print("Masukan Nama Kamu : ");
+        nama = input.nextLine();
+        System.out.println("Nama Kamu : " + nama);
+    }
+}
